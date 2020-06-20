1ba9443b623151513737265eb267a2f0adc0f14a
Jaspreet Sandhu <58150737+JaspreetSandhu@users.noreply.github.com>
2020-06-19 22:25:10 -0400

    Add files via upload

---
+++ 12.sql
@@ -0,0 +1,22 @@
+SELECT movies.title FROM movies
+
+JOIN stars
+ON movies.id = stars.movie_id
+
+JOIN people
+ON stars.person_id = people.id
+
+WHERE people.name = "Johnny Depp"
+
+AND
+
+movies.title IN(
+SELECT movies.title FROM people
+
+JOIN stars
+ON people.id = stars.person_id
+
+JOIN movies
+ON stars.movie_id = movies.id
+
+WHERE people.name = "Helena Bonham Carter");
